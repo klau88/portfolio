ccb56c97df121326c44eabe7b8c363cf693e3ae0
klau88 <kaloklau88@gmail.com>
2019-08-25 22:36:33 +0200

    add models

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function skills()
+    {
+        return $this->belongsToMany('skills', 'project_skill');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('projects', 'project_tag');
+    }
 }
--- app/Skill.php
+++ app/Skill.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function projects()
+    {
+        return $this->belongsToMany('projects', 'project_skill', 'skill_id', 'project_id');
+    }
 }
