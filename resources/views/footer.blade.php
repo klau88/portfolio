0c31291f256ac62267a5f634d7d28fadaa542c01
klau88 <kaloklau88@gmail.com>
2019-08-27 01:06:20 +0200

    add social link icons to footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,10 @@
-<footer class="container-fluid bg-primary">
+<footer class="container bg-light">
+    <div class="d-flex justify-content-around">
+        <h1><a href="https://www.linkedin.com/in/kalok-lau-a3153216/"><i class="fab fa-linkedin"></i></a></h1>
+        <h1><a href="mailto:kaloklau88@gmail.com"><i class="far fa-envelope"></i></a></h1>
+        <h1><a href="tel:+31625124441"><i class="fab fa-whatsapp"></i></a></h1>
+        <h1><a href="https://github.com/klau88"><i class="fab fa-github"></i></a></h1>
+    </div>
     <div class="row">
         <span class="m-auto">
         &copy; {{ now()->year }} {{ config('auth.default.user') }}
