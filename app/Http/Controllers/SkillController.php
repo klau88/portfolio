8a6640c6040a6bb4294217d3608a6c8d344a00f7
klau88 <kaloklau88@gmail.com>
2019-09-06 23:34:04 +0200

    update migrations and controllers

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -6,6 +6,11 @@ use App\Skill;
 
 class SkillController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,36 +1,43 @@
 @extends('layout')
 
 @section('content')
-    <form method="post" action="{{ route('projects.update', compact('project')) }}" id="project-form">
-        <div class="row mt-3">
-            <h3 class="col-sm-10 col-xs-12 text-sm-left text-center">{{ trans('form.edit') }} {{ trans('app.project') }}</h3>
-            <div class="col-sm-2 col-xs-12 text-sm-right text-center">
-                <a class="btn btn-success" href="{{ route('projects.index') }}">{{ trans('form.overview') }}</a>
-                <a class="btn btn-primary" href="{{ route('projects.show', ['id' => $project->id]) }}">{{ trans('form.show') }}</a>
-            </div>
+
+    <div class="row mt-3">
+        <h3 class="col-sm-10 col-xs-12 text-sm-left text-center">{{ trans('form.edit') }} {{ trans('app.project') }}</h3>
+        <div class="col-sm-2 col-xs-12 text-sm-right text-center">
+            <a class="btn btn-success" href="{{ route('projects.index') }}">{{ trans('form.overview') }}</a>
+            <a class="btn btn-primary" href="{{ route('projects.show', ['id' => $project->id]) }}">{{ trans('form.show') }}</a>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-sm-6 col-12">
+            <form method="post" action="{{ route('projects.update', compact('project')) }}" id="project-form">
+                @csrf
+                @method('PATCH')
+
+                @include('inputs.text', ['field' => 'title', 'value' => $project->title, 'placeholder' => trans('form.title')])
+
+                @include('inputs.textarea', ['field' => 'description', 'value' => $project->description, 'placeholder' => trans('form.description')])
+
+                @include('inputs.date', ['field' => 'start', 'value'=> $project->start, 'placeholder' => trans('form.start')])
+
+                @include('inputs.date', ['field' => 'end', 'value'=> $project->end, 'placeholder' => trans('form.end')])
+            </form>
         </div>
 
-        <hr>
+        <div class="col-sm-6 col-12">
+            @include('images.upload', ['imageableType' => 'App\Project', 'imageableId' => $project->id])
 
-        @csrf
-        @method('PATCH')
-
-        @include('inputs.text', ['field' => 'title', 'value' => $project->title, 'placeholder' => trans('form.title')])
-
-        @include('inputs.textarea', ['field' => 'description', 'value' => $project->description, 'placeholder' => trans('form.description')])
-
-        @include('inputs.date', ['field' => 'start', 'value'=> $project->start, 'placeholder' => trans('form.start')])
-
-        @include('inputs.date', ['field' => 'end', 'value'=> $project->end, 'placeholder' => trans('form.end')])
-    </form>
-
-    @include('images.upload', ['imageableType' => 'App\Project', 'imageableId' => $project->id])
-
-    @foreach($project->images as $image)
-        <div class="d-flex justify-content-around">
-            <img width="500" src="{{ asset('images/'.$image->name) }}">
+            @foreach($project->images as $image)
+                <div class="d-flex justify-content-around">
+                    <img width="100%" src="{{ asset('images/'.$image->name) }}">
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 
     <div class="form-group row">
         <div class="col-sm-10">
